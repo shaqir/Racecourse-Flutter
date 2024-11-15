9f429c052adca9ca4aaf0eff86062f86eba616c7
Sakir Saiyed <sakirsaiyed29@gmail.com>
2024-11-14 17:24:03 -0700

    Added Bottom Navigation Menu

---
+++ lib/core/appcolors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+   static const Color primaryLightPurpleColor = Color(0xffd2ceea);
+}
+
---
+++ lib/core/appconstants.dart
@@ -0,0 +1,12 @@
+class Appconstants {
+  static const String dashboardMenuItem1 = 'Home';
+  static const String dashboardMenuItem2 = 'Selection';
+  static const String dashboardMenuItem3 = 'Settings';
+  static const String dashboardMenuItem4 = 'Profile';
+}
+
+enum AppEnvironment {
+  development,
+  staging,
+  production
+}
